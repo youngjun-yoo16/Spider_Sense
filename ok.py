78343102e3c5fece6a86ee2c608f32024fcd4f92
Shelly Schwartz <shellyschwartz@dhcp-245-134.resnet.purdue.edu>
2020-10-18 09:54:06 -0400

    tuple to array

--- ok.py
+++ ok.py
@@ -5,17 +5,17 @@ except ImportError:
     # Fall back to Python 2's urllib2
     from urllib import urlopen
 
-
 import json
 import sys
 
 
-#flatearth = " https://factchecktools.googleapis.com/v1alpha1/claims:search?languageCode=en-US&pageSize=3&query=the%20earth%20is%20flat&key=AIzaSyC-PX-31ru9Y3O4RCKOwloQplLgJ2LTCl8"
+# flatearth = " https://factchecktools.googleapis.com/v1alpha1/claims:search?languageCode=en-US&pageSize=3&query=the%20earth%20is%20flat&key=AIzaSyC-PX-31ru9Y3O4RCKOwloQplLgJ2LTCl8"
 
 def get_jsonparsed_data(url):
     response = urlopen(url)
     data = response.read().decode("utf-8")
     return json.loads(data)
+
 
 def get_json_data(json):
     for claim in json["claims"]:
@@ -28,10 +28,11 @@ def get_json_data(json):
     return claimu, claiment, title, j_url, rating
 
 
-
 def main(url_json):
     json = get_jsonparsed_data(url_json)
-    return get_json_data(json)
+    a = [get_json_data(json)]
+    return a
+
 
 main(sys.argv)
 
